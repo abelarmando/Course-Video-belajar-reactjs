3c572a0fd338c1ca657f019a960641cec6a4632a
abel armando <abelarmando79@gmail.com>
2025-01-15 15:50:51 +0700

    memperbarui

---
+++ src/Components/Fragments/CardProduct.jsx
@@ -0,0 +1,27 @@
+
+
+function CardProduct() {
+
+    return (
+        <div className="">
+            <img src="./images/Icon/Cover/Product-01.svg" alt="produk" />
+            <h3>Big 4</h3>
+            <p>mulai transformasi</p>
+            <div className="">
+                <img src="./images/Icon/Avatar/Avatar-1.svg" alt="avatar" />
+                
+                <p>Jenna</p>
+                <p>Senior Accountant di <b>Gojek</b> </p>
+            </div>
+            <div className="">
+            <div class="">
+                <img src="./images/Rating.svg" alt="rating"/>
+                <span class="jumlah-rating">3.5 (86)</span>
+            </div>
+                <p class="">Rp 300K</p>   
+            </div>            
+        </div>
+    );
+}
+
+export default CardProduct
---
+++ src/pages/home.jsx
@@ -0,0 +1,10 @@
+import CardProduct from "../Components/Fragments/CardProduct";
+
+
+function home() {
+    return (
+        <CardProduct/>
+    )
+}
+
+export default home
