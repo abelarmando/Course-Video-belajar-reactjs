87f606e923e268135eccdecd48ab313d71837765
abel armando <abelarmando79@gmail.com>
2025-01-17 02:12:18 +0700

    menambahkan banner footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,10 @@ export default {
     extend: {
       backgroundImage: {
         bannerheaderImg: "url('./images/Background_Image/Background_banner.jpg')",
+        bannerfooterImg: "url('./images/Background_Image/Background_footer.jpg')",
+      },
+      gridTemplateColumns: {
+        'auto-fill': 'repeat(auto-fit, minmax(400px, 1fr))',
       },
     },
   },
