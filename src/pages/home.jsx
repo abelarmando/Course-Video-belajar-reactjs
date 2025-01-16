fa23d8de18ca70929970e82512907f135365a10e
abel armando <abelarmando79@gmail.com>
2025-01-16 15:45:13 +0700

    menambahkan home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,21 +1,34 @@
 import CardProduct from "../Components/Fragments/CardProduct";
 
 
+const produck = [
+    {
+        author: 'Jenna Ortega',
+        position: 'Senior Accountant',
+        work: 'Gojek',
+        rating: '3.5 (86)',
+        price: 'Rp 300k'
+    }
+]
+
+
 function Home() {
     return (
-       <CardProduct>
-           <CardProduct.Header 
-           img="./images/produk1.png"/>
-           <CardProduct.Body 
-           title='Big 4 Auditor Financial Analyst' 
-           author='Jenna Ortega' 
-           position='Senior Accountant' 
-           work='Gojek'
-           imgprofile="./images/avatar1.png"> 
-           Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan kurikulum terbaik
-           </CardProduct.Body>
-           <CardProduct.Footer rating='3.5 (86)' price='Rp 300k'/>
-       </CardProduct>
+        <>
+            <CardProduct>
+                <CardProduct.Header 
+                img="./images/produk1.png"/>
+                <CardProduct.Body 
+                title='Big 4 Auditor Financial Analyst' 
+                author='Jenna Ortega' 
+                position='Senior Accountant' 
+                work='Gojek'
+                imgprofile="./images/avatar1.png"> 
+                Mulai transformasi dengan instruktur profesional, harga yang terjangkau, dan kurikulum terbaik
+                </CardProduct.Body>
+                <CardProduct.Footer rating='3.5 (86)' price='Rp 300k'/>
+            </CardProduct>
+        </>
     )
 }
 
