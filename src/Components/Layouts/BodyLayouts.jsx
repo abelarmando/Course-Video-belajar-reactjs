d0fc0bd13e8f49566f49a3657cb60a15e5bca1ea
abel armando <abelarmando79@gmail.com>
2025-01-21 22:52:03 +0700

    banner saya sudah capek

--- src/Components/Layouts/BodyLayouts.jsx
+++ src/Components/Layouts/BodyLayouts.jsx
@@ -7,7 +7,7 @@ function BodyLayouts() {
   return (
     <div className="py-16 px-[120px] space-y-16 max-md:px-5">
       <Banner
-        img="bg-[url('/images/Background_Image/Background_banner.jpg');]"
+        img="bg-bannerheaderImg"
         class="w-full h-full bg-[#000000CC] rounded-xl flex flex-col items-center text-center py-20 px-72 space-y-6 text-white max-md:px-5 max-md:py-16"
       >
         <h1 className="text-4xl">
@@ -61,7 +61,7 @@ function BodyLayouts() {
       </div>
 
       <Banner
-        img="bg-[url('/images/Background_Image/Background_footer.jpg';)]"
+        img="bg-bannerfooterImg"
         class="w-full h-full bg-[#000000CC] rounded-xl flex flex-col items-center text-center py-20 px-72 space-y-6 text-white max-md:px-5 max-md:py-16"
       >
         <p>NEWSLATER</p>
