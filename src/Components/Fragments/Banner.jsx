6f664a7fca96b2e30d5ac4d92bd9377b9906c95d
abel armando <abelarmando79@gmail.com>
2025-01-21 22:48:59 +0700

    final benerin banner

--- src/Components/Fragments/Banner.jsx
+++ src/Components/Fragments/Banner.jsx
@@ -1,7 +1,7 @@
 function Banner(props) {
   const { img, children } = props;
   return (
-    <div className={` bg-repeat bg-cover w-full h-fit rounded-xl ${img}`}>
+    <div className={` bg-repeat bg-cover w-full h-fit rounded-xl ${img} `}>
       <div className={props.class}>{children}</div>
     </div>
   );
