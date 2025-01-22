a5c938dc6a4c86194466ad9cc00165608ddf6699
abel armando <abelarmando79@gmail.com>
2025-01-22 19:40:51 +0700

    membenarkan halaman registrasi

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -6,7 +6,7 @@ function Register() {
   return (
     <>
       <Navbar />
-      <main className="md:flex md:justify-center md:items-center h-screen w-full max-md:px-5 max-md:py-7">
+      <main className="md:flex md:justify-center md:items-center md:h-screen w-full max-md:px-5 max-md:py-7">
         <AuthLayouts
           title="Pendaftaran Akun"
           keterangan="Yuk, daftarkan akunmu sekarang juga!"
