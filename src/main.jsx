f8c9bfbfa15cf5c015627b49c29819629d1c38c6
abel armando <abelarmando79@gmail.com>
2025-01-31 19:41:36 +0700

    memperbarui nama

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Register from "./pages/register.jsx";
 import Home from "./pages/home.jsx";
 import Kategori from "./pages/kategori.jsx";
 import Produk from "./pages/produk.jsx";
-import Metode from "./pages/metode_.jsx";
+import Metode from "./pages/method.jsx";
 import Bayar from "./pages/bayar.jsx";
 
 const router = createBrowserRouter([
